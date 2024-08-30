bd7cfa62ab08e1761b6230ee59b8c5b462ebaa7f
joselinares17 <josedaniellinares135@gmail.com>
2024-08-30 17:48:07 -0500

    adding initial entity classes

---
+++ src/main/java/org/linaresworks/dream_shops/domain/entity/Product.java
@@ -0,0 +1,36 @@
+package org.linaresworks.dream_shops.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(
+            cascade = CascadeType.ALL, orphanRemoval = true,
+            mappedBy = "product"
+    )
+    private List<Image> images;
+}
