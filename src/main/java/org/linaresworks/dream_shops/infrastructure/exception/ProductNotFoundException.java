bccf4a9ccebcf36d23d8f72143bea3532c0ee63b
joselinares17 <josedaniellinares135@gmail.com>
2024-08-30 18:22:20 -0500

    adding first custom exception

---
+++ src/main/java/org/linaresworks/dream_shops/infrastructure/exception/ProductNotFoundException.java
@@ -0,0 +1,13 @@
+package org.linaresworks.dream_shops.infrastructure.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+    private static final String msg = "product not found";
+
+    public ProductNotFoundException() {
+        super(msg);
+    }
+
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
