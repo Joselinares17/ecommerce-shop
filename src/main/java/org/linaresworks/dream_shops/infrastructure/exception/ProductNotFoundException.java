d31ac076e03e13ee81fe3bb0000a441e2540a3bc
joselinares17 <josedaniellinares135@gmail.com>
2024-09-01 16:15:51 -0500

    modifying exception class

--- src/main/java/org/linaresworks/dream_shops/infrastructure/exception/ProductNotFoundException.java
+++ src/main/java/org/linaresworks/dream_shops/infrastructure/exception/ProductNotFoundException.java
@@ -1,6 +1,6 @@
 package org.linaresworks.dream_shops.infrastructure.exception;
 
-public class ProductNotFoundException extends RuntimeException{
+public class ProductNotFoundException extends ResourceNotFoundException{
     private static final String msg = "product not found";
 
     public ProductNotFoundException() {
