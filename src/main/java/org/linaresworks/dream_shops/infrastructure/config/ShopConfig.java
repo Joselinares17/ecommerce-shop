2761a8cc9b64a3648617bc81c32e2759226f930e
joselinares17 <josedaniellinares135@gmail.com>
2024-09-11 01:16:43 -0500

    adding modelmapper class's config

---
+++ src/main/java/org/linaresworks/dream_shops/infrastructure/config/ShopConfig.java
@@ -0,0 +1,16 @@
+package org.linaresworks.dream_shops.infrastructure.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShopConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    // Otros beans de configuración
+}
