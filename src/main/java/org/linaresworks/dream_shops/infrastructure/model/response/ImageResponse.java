fda45cb05121f1706c839ec90ded130c2af21da4
joselinares17 <josedaniellinares135@gmail.com>
2024-09-11 01:13:25 -0500

    adding new dto

--- src/main/java/org/linaresworks/dream_shops/infrastructure/model/response/ImageResponse.java
+++ src/main/java/org/linaresworks/dream_shops/infrastructure/model/response/ImageResponse.java
@@ -1,9 +1,13 @@
-package org.linaresworks.dream_shops.infrastructure.model.dto;
+package org.linaresworks.dream_shops.infrastructure.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class ImageDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageResponse {
     private Long imageId;
     private String imageName;
     private String downloadUrl;
